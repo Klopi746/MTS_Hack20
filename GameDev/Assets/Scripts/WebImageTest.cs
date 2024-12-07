94ed27a939ea4ce7606c852a24e54c1a74db4c86
Steel Bocharov <132206705+Klopi746@users.noreply.github.com>
2024-12-08 01:48:24 +0300

    1.08 - rewrite get test image script

--- GameDev/Assets/Scripts/WebImageTest.cs
+++ GameDev/Assets/Scripts/WebImageTest.cs
@@ -5,50 +5,33 @@ using System.Collections;
 
 public class WebImageTest : MonoBehaviour
 {
-    public Image uiImage; 
-    //public string imageUrl = "https://example.com/your-image.png";
-
-    private void Start()
+    public Image uiImage;
+    void Start()
     {
-        string decodedUrl = DecodeUrl(imageUrl);
-        StartCoroutine(LoadImageFromURL(decodedUrl));
+        StartCoroutine(GetTexture());
     }
 
-    private IEnumerator LoadImageFromURL(string url)
+    IEnumerator GetTexture()
     {
-        UnityWebRequest request = UnityWebRequestTexture.GetTexture("https://img.nagradion.ru/images/normal/m/person980434955.jpg");
-        request.SetRequestHeader("mode", "no-cors");
-        // UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
-        // request.SetRequestHeader("Access-Control-Allow-Credentials", "true");
-        // request.SetRequestHeader("Access-Control-Allow-Headers", "Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time");
-        // request.SetRequestHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-        // request.SetRequestHeader("Access-Control-Allow-Origin", "*");
-        yield return request.SendWebRequest();
-        if (request.result != UnityWebRequest.Result.Success)
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture("https://i.pinimg.com/originals/18/49/5f/18495fc57202045db3299834b4e27759.jpg");
+        www.SetRequestHeader("mode", "no-cors");
+        www.SetRequestHeader("Access-Control-Allow-Credentials", "true");
+        www.SetRequestHeader("Access-Control-Allow-Headers", "Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time");
+        www.SetRequestHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+        www.SetRequestHeader("Access-Control-Allow-Origin", "*");
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
         {
-            Debug.LogError($"Ошибка загрузки изображения: {request.error}");
+            Debug.LogError($"Ошибка загрузки изображения: {www.error}");
         }
         else
         {
-            Texture2D texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+            Texture2D texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
             Sprite sprite = Sprite.Create(texture,
             new Rect(0, 0, texture.width, texture.height),
             new Vector2(0.5f, 0.5f));
             uiImage.sprite = sprite;
         }
-    }
-
-    private string DecodeUrl(string url)
-    {
-        if (url.Contains("imgurl="))
-        {
-
-            string[] parts = url.Split(new string[] { "imgurl=" }, System.StringSplitOptions.None);
-            string encodedUrl = parts[1].Split('&')[0];
-
-
-            return UnityWebRequest.UnEscapeURL(encodedUrl);
-        }
-        return url;
     }
 }
