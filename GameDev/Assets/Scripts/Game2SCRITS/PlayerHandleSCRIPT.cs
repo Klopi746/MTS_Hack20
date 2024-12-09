29611eaa44c1fc516306f0ee6d253899119d4352
Steel Bocharov <132206705+Klopi746@users.noreply.github.com>
2024-12-09 22:08:52 +0300

    1.21 - add more smoothly move for player in game2

--- GameDev/Assets/Scripts/Game2SCRITS/PlayerHandleSCRIPT.cs
+++ GameDev/Assets/Scripts/Game2SCRITS/PlayerHandleSCRIPT.cs
@@ -8,8 +8,8 @@ public class PlayerHandleSCRIPT : MonoBehaviour
     bool isAlive = true;
     public bool canDie = true;
     public float timeBetweenMove = 0.5f;
-    public float speedOfMove = 0.05f;
-    public float sizeOfMove = 0.1f;
+    public float speedOfMove = 0.01f;
+    public float sizeOfMove = 0.02f;
     public float delayBeforeFirstMove = 2f;
 
     Vector3 curPosition;
@@ -50,7 +50,7 @@ public class PlayerHandleSCRIPT : MonoBehaviour
             isMoving = true;
             while (transform.position != newPosition)
             {
-                transform.position = (transform.position + transform.up * sizeOfMove).Round(1);
+                transform.position = (transform.position + transform.up * sizeOfMove).Round(2);
                 yield return new WaitForSeconds(speedOfMove);
             }
             isMoving = false;
