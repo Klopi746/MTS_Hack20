bf97a0a8ba137c0d543d0906a143a3bfdbbd7c51
Steel Bocharov <132206705+Klopi746@users.noreply.github.com>
2024-12-09 20:07:31 +0300

    1.18 - fullfill game2 logic

--- GameDev/Assets/Scripts/Game2SCRITS/Game2ManagerSCRIPT.cs
+++ GameDev/Assets/Scripts/Game2SCRITS/Game2ManagerSCRIPT.cs
@@ -4,10 +4,16 @@ using UnityEngine.SceneManagement;
 public class Game2ManagerSCRIPT : MonoBehaviour
 {
     public static Game2ManagerSCRIPT Instance;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public bool isGameStarted = false;
+
+    void Awake()
     {
         Instance = this;
+    }
+    void Start()
+    {
+
     }
 
     public void DieHandler()
--- GameDev/Assets/Scripts/Game2SCRITS/PlayerHandleSCRIPT.cs
+++ GameDev/Assets/Scripts/Game2SCRITS/PlayerHandleSCRIPT.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerHandleSCRIPT : MonoBehaviour
 {
     bool isAlive = true;
+    public bool canDie = true;
     public float timeBetweenMove = 0.5f;
-    public float speedOfMove = 0.1f;
+    public float speedOfMove = 0.05f;
+    public float sizeOfMove = 0.1f;
     public float delayBeforeFirstMove = 2f;
 
     Vector3 curPosition;
@@ -16,6 +19,13 @@ public class PlayerHandleSCRIPT : MonoBehaviour
 
     int screenWidth = Screen.width;
 
+    void Awake()
+    {
+#if !UNITY_EDITOR
+        isAlive = true;
+#endif
+    }
+
     void Start()
     {
         StartCoroutine(PlayerHandler());
@@ -24,28 +34,58 @@ public class PlayerHandleSCRIPT : MonoBehaviour
     IEnumerator PlayerHandler()
     {
         yield return new WaitForSeconds(delayBeforeFirstMove);
+        Game2ManagerSCRIPT.Instance.isGameStarted = true;
+
+        Transform lastTileTransform = Physics2D.Raycast(transform.position, Vector3.back * 10f).transform;
+
         while (isAlive)
         {
             curPosition = transform.position;
             newPosition = transform.position + transform.up;
+
             isMoving = true;
             while (transform.position != newPosition)
             {
-                transform.position = (transform.position + transform.up * speedOfMove).Round(1);
+                transform.position = (transform.position + transform.up * sizeOfMove).Round(1);
                 yield return new WaitForSeconds(speedOfMove);
             }
             isMoving = false;
+
             RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.back * 10f);
+#if UNITY_EDITOR
+            if (!canDie)
+            {
+                if(hit)
+                {
+                    hit.transform.GetComponent<SpriteRenderer>().color = Color.grey;
+                    Destroy(lastTileTransform.gameObject, 0.1f);
+                    TileMapBuilderSCRIPT.Instance.ChangePathCurLengthBy(-1);
+                    lastTileTransform = hit.transform;
+                }
+                yield return new WaitForSeconds(timeBetweenMove);
+                continue;
+            }
+#endif
             if (hit.collider == null)
             {
                 isAlive = false;
                 Debug.Log("You died!");
                 Game2ManagerSCRIPT.Instance.DieHandler();
             }
+            else
+            {
+                hit.transform.GetComponent<SpriteRenderer>().color = Color.grey;
+                Destroy(lastTileTransform.gameObject, 0.1f);
+                TileMapBuilderSCRIPT.Instance.ChangePathCurLengthBy(-1);
+                lastTileTransform = hit.transform;
+            }
+
             yield return new WaitForSeconds(timeBetweenMove);
         }
     }
 
+    Vector3 LEFTROTATION = new Vector3(0, 0, 90);
+    Vector3 RIGHTROTATION = new Vector3(0, 0, -90);
     void Update()
     {
         if (Input.GetMouseButtonDown(0) && !isMoving)
@@ -54,13 +94,13 @@ public class PlayerHandleSCRIPT : MonoBehaviour
 
             if (clickPosition.x < screenWidth / 2)
             {
+                if (transform.eulerAngles != LEFTROTATION) transform.eulerAngles += LEFTROTATION;
                 Debug.Log("Повернули налево");
-                transform.eulerAngles += new Vector3(0, 0, -90);
             }
             else
             {
-                Debug.Log("Повернули напрво");
-                transform.eulerAngles += new Vector3(0, 0, -90);
+                if (transform.eulerAngles != RIGHTROTATION) transform.eulerAngles += RIGHTROTATION;
+                Debug.Log("Повернули направо");
             }
         }
     }
