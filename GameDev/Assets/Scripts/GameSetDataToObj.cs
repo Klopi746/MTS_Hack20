6d8040b00634815c12f07a69532822f7a5f912cc
Steel Bocharov <132206705+Klopi746@users.noreply.github.com>
2024-12-12 03:01:45 +0300

    1.59 - fix to DataLoader for game1

--- GameDev/Assets/Scripts/GameSetDataToObj.cs
+++ GameDev/Assets/Scripts/GameSetDataToObj.cs
@@ -8,7 +8,7 @@ public class GameSetDataToObj : MonoBehaviour
     public SerializableDictionary<string, GameObject> DataToSet = new SerializableDictionary<string, GameObject>();
     public void SetData()
     {
-        foreach(System.Collections.Generic.KeyValuePair<string, GameObject> kvp in DataToSet.Dictionary)
+        foreach (System.Collections.Generic.KeyValuePair<string, GameObject> kvp in DataToSet.Dictionary)
         {
             if (!DataLoaderSCRIPT.Instance.DataToUse.ContainsKey(kvp.Key)) continue;
             if (kvp.Key.Contains("Sprite", StringComparison.OrdinalIgnoreCase))
@@ -28,13 +28,21 @@ public class GameSetDataToObj : MonoBehaviour
                 byte r = Convert.ToByte(colorString.Substring(0, 2), 16); // Красный
                 byte g = Convert.ToByte(colorString.Substring(2, 2), 16); // Зеленый
                 byte b = Convert.ToByte(colorString.Substring(4, 2), 16); // Синий
-                Color newColor = new Color(r,g,b);
+                Color newColor = new Color(r, g, b);
+                if (kvp.Key.Contains("platformColorFirst", StringComparison.OrdinalIgnoreCase))
+                {
+                    newColor = new Color(r, g, b, 0f);
+                }
+                else if (kvp.Key.Contains("platformColorSecond", StringComparison.OrdinalIgnoreCase))
+                {
+                    newColor = new Color(r, g, b, 0f);
+                }
                 SpriteRenderer colorRendererComponent = kvp.Value.GetComponent<SpriteRenderer>();
                 Image colorImageComponent = kvp.Value.GetComponent<Image>();
                 ParticleSystem colorParticalSystem = kvp.Value.GetComponent<ParticleSystem>();
                 if (colorRendererComponent != null) colorRendererComponent.color = newColor;
-                else if(colorImageComponent) colorImageComponent.color = newColor;
-                else if(colorParticalSystem) colorParticalSystem.startColor = newColor;
+                else if (colorImageComponent) colorImageComponent.color = newColor;
+                else if (colorParticalSystem) colorParticalSystem.startColor = newColor;
                 else Debug.Log($"Я незнаю что это за компонент у объекта {kvp.Value}. Укажи Мне!");
             }
             if (kvp.Key.Contains("difficulty", StringComparison.OrdinalIgnoreCase))
