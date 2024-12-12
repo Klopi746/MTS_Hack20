448d29d0c2ba4296343931454cafd866a9d6239b
Steel Bocharov <132206705+Klopi746@users.noreply.github.com>
2024-12-12 03:35:18 +0300

    1.62 - game1 & game2 data loading ready

--- GameDev/Assets/Scripts/DataLoaderSCRIPT.cs
+++ GameDev/Assets/Scripts/DataLoaderSCRIPT.cs
@@ -26,21 +26,55 @@ public class DataLoaderSCRIPT : MonoBehaviour
     }
     IEnumerator LoadConfig()
     {
-        yield return config.GetCurrentActiveConfig<BallsOfFateAttributesOut>(GameName,
-            config =>
+        if (GameName == "CubeRun")
         {
-            Debug.Log($"Configs loaded: {config}");
-            SetData(config);
-        },
-            error =>
-        {
-            Debug.LogError($"Error loading configs: {error}");
+            yield return config.GetCurrentActiveConfig<PathOfHeroAttributesOut>(GameName,
+                config =>
+            {
+                Debug.Log($"Configs loaded: {config}");
+                SetPathOfHeroData(config);
+            },
+                error =>
+            {
+                Debug.LogError($"Error loading configs: {error}");
+            }
+            );
         }
-        );
+        else
+        {
+            yield return config.GetCurrentActiveConfig<BallsOfFateAttributesOut>(GameName,
+                config =>
+            {
+                Debug.Log($"Configs loaded: {config}");
+                SetBallsOfFateData(config);
+            },
+                error =>
+            {
+                Debug.LogError($"Error loading configs: {error}");
+            }
+            );
+        }
     }
-    void SetData(ConfigurationOut<BallsOfFateAttributesOut> config)
+    void SetBallsOfFateData(ConfigurationOut<BallsOfFateAttributesOut> config)
     {
         var keys = typeof(BallsOfFateAttributesOut).GetFields();
+        var attributes = config.configuration;
+        foreach(var key in keys)
+        {
+            var value = key.GetValue(attributes);
+            if (value == null) continue;
+            var valueStr = value.ToString();
+            Debug.Log(key.Name + ":" + valueStr);
+            DataToUse.Add(key.Name, valueStr);
+        }
+        // string name = config.configuration.backgroundSprite;
+        // DataToUse.Add(name, name);
+        SetDataSCRIPT.Invoke("SetData", 0f);
+    }
+
+    void SetPathOfHeroData(ConfigurationOut<PathOfHeroAttributesOut> config)
+    {
+        var keys = typeof(PathOfHeroAttributesOut).GetFields();
         var attributes = config.configuration;
         foreach(var key in keys)
         {
