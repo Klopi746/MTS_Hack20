2e1b056ac1ffb8128f2bb912ee714e869bf68b99
Steel Bocharov <132206705+Klopi746@users.noreply.github.com>
2024-12-12 05:06:03 +0300

    1.68 - fix back color

--- GameDev/Assets/Scripts/GameSetDataToObj.cs
+++ GameDev/Assets/Scripts/GameSetDataToObj.cs
@@ -28,21 +28,21 @@ public class GameSetDataToObj : MonoBehaviour
                 byte r = Convert.ToByte(colorString.Substring(0, 2), 16); // Красный
                 byte g = Convert.ToByte(colorString.Substring(2, 2), 16); // Зеленый
                 byte b = Convert.ToByte(colorString.Substring(4, 2), 16); // Синий
-                Color newColor = new Color(r, g, b);
+                Color newColor = new Color32(r, g, b, 255);
                 if (kvp.Key.Contains("platformColorFirst", StringComparison.OrdinalIgnoreCase))
                 {
-                    newColor = new Color(r, g, b, 0f);
+                    newColor = new Color32(r, g, b, 0);
                 }
                 else if (kvp.Key.Contains("platformColorSecond", StringComparison.OrdinalIgnoreCase))
                 {
-                    newColor = new Color(r, g, b, 0f);
+                    newColor = new Color32(r, g, b, 0);
                 }
                 SpriteRenderer colorRendererComponent = kvp.Value.GetComponent<SpriteRenderer>();
                 Image colorImageComponent = kvp.Value.GetComponent<Image>();
                 ParticleSystem colorParticalSystem = kvp.Value.GetComponent<ParticleSystem>();
                 if (colorRendererComponent != null) colorRendererComponent.color = newColor;
-                else if (colorImageComponent) colorImageComponent.color = newColor;
-                else if (colorParticalSystem) colorParticalSystem.startColor = newColor;
+                else if (colorImageComponent != null) colorImageComponent.color = newColor;
+                else if (colorParticalSystem != null) colorParticalSystem.startColor = newColor;
                 else Debug.Log($"Я незнаю что это за компонент у объекта {kvp.Value}. Укажи Мне!");
             }
             if (kvp.Key.Contains("difficulty", StringComparison.OrdinalIgnoreCase))
